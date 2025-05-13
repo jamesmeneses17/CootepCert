f389f12876466b2db18f4fe280665e4bf4ebf783
James Meneses <124942205+jamesmeneses17@users.noreply.github.com>
2025-05-12 23:27:16 -0500

    chore(docs): integrar Swagger para documentación interactiva de la API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Crea la aplicacion NestJS
@@ -13,11 +14,20 @@ async function bootstrap() {
 
   // Usa una validacion de datos para las peticiones
   //  transforma los datos a un formato adecuado
-
   app.useGlobalPipes(new ValidationPipe());
 
-  // Inicia el servidor en el puerto 3000
+  // Configura Swagger para documentar la API
+  const config = new DocumentBuilder()
+    .setTitle('CootepCert API')
+    .setDescription('Documentación de la API para autenticación y certificados laborales')
+    .setVersion('1.0')
+    .addTag('auth') // puedes usar varios tags como empleados, certificados, etc.
+    .build();
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Esto crea la ruta http://localhost:3000/api
+
+  // Inicia el servidor en el puerto 3000
   await app.listen(3000);
 }
 bootstrap();
